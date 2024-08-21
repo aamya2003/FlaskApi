2c65510ebf435c800f197cd6e8c6801978bde933
Ahmtr <ahmadasaadmajed1@gmail.com>
2024-08-21 17:44:26 +0300

    الشروط - 1

---
+++ cond1.py
@@ -0,0 +1,22 @@
+
+
+age_acc = 30
+
+
+
+age = int(   input("whts u age? ")    )
+
+
+
+#  == 1-same type 2- same word
+
+  
+# 30 <= age < 80
+
+if age >=30 and age < 80:
+    print("u r acc.")
+else:
+    print("u r not ecc.")
+
+
+
